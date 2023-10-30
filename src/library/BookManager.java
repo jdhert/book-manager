7c06adf73478e409aeef38649c17b112e09b47ec
kitri <kkuldong12@gmail.com>
2023-10-30 16:44:44 +0900

    first commit

---
+++ src/library/BookManager.java
@@ -0,0 +1,8 @@
+package library;
+
+public abstract class BookManager {
+    abstract void addBook(Book book);
+    abstract void printAllBook();
+    abstract void updateBook(Book book);
+    abstract void removeBook(Book book);
+}
